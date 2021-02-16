7fcf0b208f88e7deff386b0c45e065ea350c64b5
Wizock <36351054+Wizock@users.noreply.github.com>
2021-02-16 23:27:35 +1000

    Add files via upload

---
+++ arrays.py
@@ -0,0 +1,93 @@
+multiAns1 = ['is not important','Not harmful to the human body','Putting on clean clothes after <br> running hard<br> at your soccer game','Improved self-esteem', 'Caring for and cleaning our bodies<br> to promote good health and happiness','Ice cubes in a tray',
+             'Attacks','Food is easier to remove from your<br> mouth at those times','Viruses replicating themselves','0 degrees fahrenheit','It makes your teeth more brittle',
+             'Immune system','Most food has germs in it','Teens are more likely to get dirty'
+             ]
+
+multiAns2 = ['Does not help with keeping us well',
+             'Very small, microscopic living things<br> that can enter your body and make you sick',
+             'Eating without washing your hands',
+             'better heath',
+             'Washing our hands',
+             'Cars in a parking lot',
+             'Ignores',
+             "That's when most people<br> eat the most",
+             'Bacteria digesting food',
+             '32 degrees fahrenheit',
+             'It makes your nails too short',
+             'Circulatory system',
+             'You may not have enough napkins',
+             'Teens have oilier skin'
+             ]
+multiAns3 = ['Is a collection of habits people perform<br>to keep themselves<br> clean and their bodies healthy',
+             'Not alive',
+             'Brushing your teeth once a month',
+             'Others will have a<br> better perception of you',
+             'Doing something<br> to improve our self-esteem',
+             'Trees in a park',
+             'Nurtures',
+             "It's easier to remember<br> to do at those times",
+             'Skin glands secreting oils',
+             '100 degrees fahrenheit',
+             'It transfers bacteria between<br> your mouth and hands',
+             'Nervous system',
+             'You might touch your<br> mouth while you eat',
+             'Teens have a stronger sense of smell'
+             ]
+
+multiAns4 = ['Increases the number of<br> germs that enter our bodies',
+             'So big that you can obviously see them and<br> prevent them from entering your body and<br> making you sick',
+             'Not taking a shower after you <br> play outside in the mud',
+             'All of the answers are correct.',
+             'All of the answers are correct.',
+             'Grains of sand on a beach',
+             'Multiplies',
+             'Bacteria continue to digest<br> food in your mouth at night',
+             'A reaction between oxygen<br> and body secretion',
+             '212 degrees fahrenheit',
+             'It prevents bacteria from<br> digesting food in your mouth',
+             'Respiratory system',
+             'The food may not have been<br> prepared in a clean environment',
+             'Teens are more concerned about<br> their appearance'
+             ]
+
+questions = ["(fill in the blank) Personal hygiene___",
+             "(fill in the blank) Germs are ____",
+             'Which of the following is an example of having good personal hygiene?',
+             'What is a benefit of practicing good personal hygiene?',
+             "What do we mean when we say 'good personal hygiene'?",
+             'The number of germs on your hands is comparable to the number of',
+             '(fill in the blank) Good personal hygiene _____ harmful germs on your body',
+             'Which is the most likely reason for brushing your teeth in<br> the morning and before bed?',
+             'Bad breath and body odor are produced by:',
+             'Which water temperature is ideal for washing your hands?',
+             'How can biting your nails be harmful to your health?',
+             'Poor hygiene has the most direct effect on which body system?',
+             'Why is it important to wash your hands before meals?',
+             'Teenagers should probably shower more often than younger kids because:'
+             ]
+
+questionsX = [515, 515, 148, 352, 352, 180, 90, 384, 464, 308, 308, 260, 372, 94]
+questionsY = [157, 157, 162, 162, 162, 168, 160, 122, 166, 158, 158, 158, 158, 172]
+ans1X = [238, 159, 159, 136, 218, 108, 260, 134, 160, 200, 150, 220, 188, 154]
+ans2X = [144, 89, 150, 136, 264, 236, 252, 186, 192, 192, 164, 216, 132, 228]
+ans3X = [1273, 1464, 1328, 1320, 1378, 1358, 1450, 1376, 1382, 1378, 1350, 1408, 1376, 1302]
+ans4X = [1370, 1355, 1346, 1346, 1346, 1346, 1460, 1350, 1364, 1382, 1334, 1398, 1314, 1336]
+ans1Y = [539, 539, 539, 528, 538, 528, 540, 534, 544, 544, 542, 542, 546, 544]
+ans2Y = [884, 868, 882, 880, 876, 882, 884, 860, 876, 876, 880, 880, 880]
+ans3Y = [518, 544, 554, 552, 530, 546, 538, 552, 550, 538, 544, 526, 554]
+ans4Y = [873, 873, 866, 866, 888, 888, 892, 870, 874, 888, 862, 884, 870, 872]
+
+xArraySoap = [70, 70, 70, 70, 300, 520, 520, 520, 520, 520, 750, 970, 970, 970, 970, 970, 1190, 1410, 1410, 1410, 1410,1410]
+yArraySoap = [330, 510, 700, 880, 880, 880, 700, 510, 330, 140, 140, 140, 330, 510, 700, 880, 880, 880, 700, 510, 300,140]
+
+xArrayVirus = [70, 70, 70, 300, 520, 520, 520, 520, 520, 750, 970, 970, 970, 970, 970, 1190, 1410, 1410, 1410, 1410,141]
+yArrayVirus = [480, 660, 850, 850, 850, 670, 480, 300, 110, 110, 110, 290, 480, 660, 850, 850, 850, 670, 480, 290, 110]
+
+xArray = [88, 88, 88, 88, 88, 313, 543, 543, 543, 543, 543, 758, 983, 983, 983, 983, 983, 1208, 1423, 1423, 1423, 1423,1423, 1640]
+yArray = [110, 300, 480, 670, 855, 855, 855, 670, 480, 300, 110, 110, 110, 300, 480, 670, 855, 855, 855, 670, 480, 300,110, 90]
+
+correct_Answers = [3,2,1,4,4,4,1,4,2,3,3,1,1,1]
+
+quizDispInd = 0
+count = 0
+score = 0
